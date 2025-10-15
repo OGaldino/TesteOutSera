b8e6d96fbf270d11e1e3266dcc9e0807c97a10de
Ozéas Galdino <ozeassilva92@hotmail.com>
2025-10-15 10:39:23 -0300

    Ajuste para teste apenas de API

--- cucumber.js
+++
@@ -1,5 +0,0 @@
-// cucumber.js
-module.exports = {
-  default: `--require-module ts-node/register --require src/steps/**/*.ts src/features/**/*.feature --format json:reports/cucumber_report.json`,
-  // Note que 'src/features/**/*.feature' foi adicionado.
-};
--- src/pages/BasePage.ts
+++
@@ -1,9 +0,0 @@
-import { Page } from '@playwright/test';
-
-export class BasePage {
-  constructor(protected page: Page) {}
-
-  async navigateTo(url: string) {
-    await this.page.goto(url);
-  }
-}
